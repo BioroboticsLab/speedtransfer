e29dee73f9fae30bcb982bc13d6122543d3e0185
mellej98 <julia.mellert@gmx.de>
2024-03-26 12:08:54 +0100

    aggreagtes side1

--- analysis/aggregate_cosinor_estimates_per_hive_position.sh
+++ analysis/aggregate_cosinor_estimates_per_hive_position.sh
@@ -16,11 +16,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 
-#SBATCH --mem=2G
+#SBATCH --mem=50G
 #SBATCH --cpus-per-task=1
 #SBATCH --threads-per-core=1
 
 source /home/juliam98/.bashrc
 source activate speedtransfer
 
-srun --unbuffered python aggregate_cosinor_estimates_per_hive_position.py --year 2016 --side 0
+srun --unbuffered python aggregate_cosinor_estimates_per_hive_position.py --year 2016 --side 1
